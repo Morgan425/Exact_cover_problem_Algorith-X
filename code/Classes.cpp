8be93bac38b751a994d4c08edd38624e0ba519e6
Morgan Aubert <morganaubert425@gmail.com>
2025-06-30 15:29:54 +0200

    Theoretically complete matrix to node matrix constructor

--- code/Classes.cpp
+++ code/Classes.cpp
@@ -38,16 +38,14 @@ class Diagram {
         Header = new Data_object();
 
         // Initialize the nodes matrix
-
-        std::vector<std::vector<Data_object>> data_objects(matrix.size() + 1, std::vector<Data_object>(matrix[0].size()));
-        for (int i = 0; i < matrix.size(); i++) {
-            for (int j = 1; j <= matrix[i].size(); j++) {
+        std::vector<std::vector<Data_object*>> data_objects(matrix.size() + 1, std::vector<Data_object*>(matrix[0].size()));
+        for (int i = 1; i <= matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
                 data_objects[i][j] = new Data_object();
             }
         }
 
         // Fill the first row with Column_object instances and links them
-
         for (int j = 0; j < matrix[0].size(); j++) {
             Column_object* new_column_object = new Column_object(std::string("C") + std::to_string(j), 0);
             if (j == 0) {
@@ -65,11 +63,28 @@ class Diagram {
         }
 
         // Vertically link nodes
-
         for (int j = 0; j < matrix[0].size(); j++) {
-             
+            Column_object* column = data_objects[0][j];
+            for (int i = 1; i <= matrix.size(); i++) {
+                data_objects[i][j]->Column_header = column;
+                data_objects[i][j]->Up = data_objects[i-1][j];
+                data_objects[i-1][j]->Down = data_objects[i][j];
+            }
+            int i = data_objects.size() - 1;
+            data_objects[i][j]->Down = column;
+            column->Up = data_objects[i][j];
+        }
+
+        // Horizontally link nodes
+        for (int i = 1; i <= matrix.size(); i++) {
+            for (int j = 1; j < matrix.size(); j++) {
+                data_objects[i][j]->Left = data_objects[i][j-1];
+                data_objects[i][j-1]->Right = data_objects[i][j];
+            }
+            int j = data_objects[0].size() - 1;
+            data_objects[i][j]->Right = data_objects[i][0];
+            data_objects[i][0]->Left = data_objects[i][j];
         }
     }
+
 }
-
-
